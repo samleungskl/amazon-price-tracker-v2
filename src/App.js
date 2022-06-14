4410757e191011bd8985b063371125870407f29c
samleung <samleung@samleungs-Virtual-Machine.local>
2022-06-14 03:26:06 -0700

    table for price history

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import useApplicationData from './hooks/useApplicationData';
 
 function App() {
   const { state } = useApplicationData();
+  console.log(state)
 return (
   <div className="App">
     <h1>Amazon Price Tracker V2</h1>
     <h2>Data is fetched at 12:00am PST everyday </h2>
-    <AsinList asinList={state.asin} />
+    <AsinList asinList={state.asin} dataList={state.data}/>
   </div>
 );
 }
--- src/AsinList/AsinList.js
+++ src/AsinList/AsinList.js
@@ -1,16 +1,22 @@
 import './AsinList.scss';
 import AsinListItem from './AsinListItem';
-function AsinList({ asinList }) {
-    const result = asinList.map((element) => {
-        return <AsinListItem 
-        asin={element.asin} 
-        asinCurrentPrice={element.asinCurrentPrice} 
-        asinDesiredPrice={element.asinDesiredPrice}
-        asinFullUrl={element.asinFullUrl}
-        asinImageUrl={element.asinImageUrl}
-        asinLastUpdate={element.asinLastUpdate}
-        asinName={element.asinName}
-        key={element.asinUid} />
+import PriceHistoryTable from '../PriceHistoryTable/PriceHistoryTable';
+function AsinList({ asinList, dataList }) {
+    const result = asinList.map((element, index) => {
+        const filteredData = dataList.filter((data) =>
+            data.dataAsin === element.asin
+        )
+        return <div className="AsinListAndTable" key={element.asinUid}>
+            <AsinListItem
+            asin={element.asin}
+            asinCurrentPrice={element.asinCurrentPrice}
+            asinDesiredPrice={element.asinDesiredPrice}
+            asinFullUrl={element.asinFullUrl}
+            asinImageUrl={element.asinImageUrl}
+            asinLastUpdate={element.asinLastUpdate}
+            asinName={element.asinName} />
+            <PriceHistoryTable dataArray={filteredData} />
+        </div>
     })
     return (
         <div className="AsinList">
