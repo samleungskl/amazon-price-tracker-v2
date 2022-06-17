722b9c7a0de4360648fff6178659de16a55adcf7
samleung <samleung@samleungs-Virtual-Machine.local>
2022-06-17 06:53:12 -0700

    update to run at 12pm daily

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
 return (
   <div className="App">
     <h1>Amazon Price Tracker V2</h1>
-    <h2>Data is fetched at 12:00am PST everyday </h2>
     <AsinList asinList={state.asin} dataList={state.data}/>
   </div>
 );
