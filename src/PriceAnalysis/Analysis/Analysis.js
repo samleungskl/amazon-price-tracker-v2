f6e817e53cd4769b859f6aab2410927801011edb
samleung <samleung@samleungs-Virtual-Machine.local>
2022-08-03 06:02:06 -0700

    additional info on price analysis page

--- src/PriceAnalysis/Analysis/Analysis.js
+++ src/PriceAnalysis/Analysis/Analysis.js
@@ -1,8 +1,12 @@
 import './Analysis.scss';
 import AnalysisCard from './AnalysisCard';
-function Analysis({min, max, mean}) {
+function Analysis({min, max, mean,current, desire}) {
     return (
         <div className="Analysis">
+            <h4 className='analysisSubtitle'>Current</h4>
+            <AnalysisCard title={''} value={current}/>
+            <h4 className='analysisSubtitle'>Target</h4>
+            <AnalysisCard title={''} value={desire}/>
             <h4 className='analysisSubtitle'>Min</h4>
             <AnalysisCard title={'All time'} value={min}/>
             <h4 className='analysisSubtitle'>Max</h4>
--- src/PriceAnalysis/PriceAnalysis.js
+++ src/PriceAnalysis/PriceAnalysis.js
@@ -3,18 +3,26 @@ import LineChart from './LineChart/LineChart';
 import './PriceAnalysis.scss';
 import PriceHistoryTable from './PriceHistoryTable/PriceHistoryTable';
 
-function PriceAnalysis({ asinArray, changeValueFn, priceAnalysisTab}) {
+function PriceAnalysis({ asinArray, changeValueFn, priceAnalysisTab }) {
+    console.log(asinArray)
     return (
         <div className="PriceAnalysis">
-            <a className='backButton' onClick={()=>{changeValueFn('currentPage','home')}}>Back</a>
-            <h3 className='appSubtitle'>Price Analysis</h3>
-            <LineChart dataArray={asinArray.data} asin={asinArray.asin} className='LineChartContainer'/>
-            <div className='tabButtonsContainer'>
-            <a className='tabButton' onClick={()=>{changeValueFn('priceAnalysisTab','analysis')}}>Analysis</a>
-            <a className='tabButton' onClick={()=>{changeValueFn('priceAnalysisTab','data')}}>Data</a>
+            <a className='backButton' onClick={() => { changeValueFn('currentPage', 'home') }}>Back</a>
+            <div className='asinInfoContainer'>
+                <div className='asinNameFromAnalysis'>{asinArray.asinName}</div>
             </div>
-            {priceAnalysisTab === 'data' && <PriceHistoryTable dataArray={asinArray.data}/>}
-            {priceAnalysisTab === 'analysis' && <Analysis min={asinArray.asinPriceMin} max={asinArray.asinPriceMax} mean={asinArray.asinPriceMean}/>}
+            <div className='LineChartContainer'>
+                <LineChart dataArray={asinArray.data} asin={asinArray.asin} />
+            </div>
+            <div className='tabButtonsContainer'>
+                <a className='tabButton' onClick={() => { changeValueFn('priceAnalysisTab', 'analysis') }}>Analysis</a>
+                <a className='tabButton' onClick={() => { changeValueFn('priceAnalysisTab', 'data') }}>Data</a>
+            </div>
+
+            {priceAnalysisTab === 'data' && <PriceHistoryTable dataArray={asinArray.data} />}
+            {priceAnalysisTab === 'analysis' && <Analysis min={asinArray.asinPriceMin} max={asinArray.asinPriceMax} mean={asinArray.asinPriceMean} current={asinArray.asinCurrentPrice} desire={asinArray.asinDesiredPrice} />}
+            <div className='asinCreatedDateFromAnalysis'>Created: {asinArray.asinCreatedDate} </div>
+            <div className='asinLastUpdateDateFromAnalysis'>Last Update: {asinArray.asinLastUpdate} </div>
         </div>
     );
 }
